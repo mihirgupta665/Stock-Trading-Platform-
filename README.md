86c126721dc109abbccc43552e91339048d7c7ba
Mihir Gupta <mihirgupta665@gmail.com>
2026-02-04 22:23:51 +0530

    Creating Routes from differnt section component of websites

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ i> Analysis of website design
 ii> Breaking the website into components on basic of feature
 iii> creating folders for each major componet and corresponding files for each nested minor related corresponding components
 iv> structuring and ordering each minor components(with h1) of one major component into the wraping of major compopent 
-
+v> react-router-dom : was used to make the the movement to different section of the page possible by importing { BrowserRouter, Routes, Route}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from './landing_page/home/HomePage';
+import SignUp from './landing_page/signup/SignUp';
+import About from './landing_page/about/About';
+import Pricing from './landing_page/pricing/Pricing';
+import Products from './landing_page/products/Products';
+import Support from './landing_page/support/Support';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <HomePage />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />} /> 
+            <Route path="/signup" element={<SignUp />} /> 
+            <Route path="/about" element={<About />} /> 
+            <Route path="/products" element={<Products />} /> 
+            <Route path="/pricing" element={<Pricing />} /> 
+            <Route path="/support" element={<Support />} /> 
+        </Routes>
+    </BrowserRouter>
 );
 
 
