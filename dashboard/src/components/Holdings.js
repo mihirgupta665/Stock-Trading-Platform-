c018ace025f265c21e6c0062caa1264bc70af59f
Mihir Gupta <mihirgupta665@gmail.com>
2026-02-06 14:45:15 +0530

    created holdings component

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -26,15 +26,15 @@ const Holdings = () => {
             const dayClass = stock.isLoss?"loss":"profit";
 
             return(
-              <tr key={index} className="item">
-                <th>Instrument</th>
-                <th>Qty.</th>
-                <th>Avg. cost</th>
-                <th>LTP</th>
-                <th>Cur. val</th>
-                <th>P&L</th>
-                <th>Net chg.</th>
-                <th>Day chg.</th>
+              <tr key={index} >
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>  {/* toFixed(x) sets the number of x decimals to be visible */}
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{curValue.toFixed(2)}</td>
+                <td className={profClass}>{(curValue- stock.avg*stock.qty).toFixed(2)}</td>
+                <td className={profClass}>{stock.net}</td>
+                <td className={dayClass}>{stock.day}</td>
               </tr>
             );
           })}
