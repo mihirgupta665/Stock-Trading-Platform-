6535ddc085d25b181490ceb72527c42dfe690cdc
Mihir Gupta <mihirgupta665@gmail.com>
2026-02-06 14:04:28 +0530

    creating the table of holdings

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { holdings } from "../data/data";  
 
 const Holdings = () => {
   return (
     <>
-      <h3 className="title">Holdings (13)</h3>
+      <h3 className="title">Holdings ({holdings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -17,6 +18,27 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+
+          {holdings.map((stock, index) => {
+            const curValue = stock.price * stock.qty;
+            const isProfit = (curValue - (stock.avg * stock.qty)) >= 0.0;
+            const profClass = isProfit?"profit":"loss";
+            const dayClass = stock.isLoss?"loss":"profit";
+
+            return(
+              <tr key={index} className="item">
+                <th>Instrument</th>
+                <th>Qty.</th>
+                <th>Avg. cost</th>
+                <th>LTP</th>
+                <th>Cur. val</th>
+                <th>P&L</th>
+                <th>Net chg.</th>
+                <th>Day chg.</th>
+              </tr>
+            );
+          })}
+
         </table>
       </div>
 
